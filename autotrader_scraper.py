b6dba86e064a946b908779ef030c590e69a024fd
Jon Pring <obmud3@gmail.com>
2022-09-10 00:38:23 +0100

    Outputs updated to dictionaries

--- autotrader_scraper.py
+++ autotrader_scraper.py
@@ -14,16 +14,16 @@ class Autotrader_scraper:
     def __init__(self):
         self.url = "https://www.autotrader.co.uk"
         self.delay = 10
-
         self.driver = webdriver.Safari()
         self.driver.implicitly_wait(0.5)
         self.driver.maximize_window()
         self.driver.get(self.url)
         print(f"Navigated to {self.url}")
+        self.__accept_cookies()
 
     def __sleep(self, duration = 1, distribution=0.2):
         time.sleep(duration + uniform(-distribution, distribution))
-    def accept_cookies(self):
+    def __accept_cookies(self):
         """
         Accept cookies on Autotrader homepage
         """
@@ -37,6 +37,59 @@ class Autotrader_scraper:
             self.__sleep(1)
         except:
             print(f"Error accepting cookies")  
+    
+    def __parse_vehicle_list(self):
+        vehicle_list = []
+        vehicles = self.driver.find_elements(by=By.XPATH, value="//li[@class='search-page__result']")
+        for vehicle in vehicles:
+            vehicle_standout = vehicle.find_elements(by=By.XPATH, value="span")
+            if vehicle_standout and vehicle_standout[0].text == 'Ad':
+                continue
+            else:
+                vehicle_href = vehicle.find_element(by=By.XPATH, value="article/a").get_attribute('href')
+                vehicle_id = re.findall('[0-9]{15}', vehicle_href)[0]
+                vehicle_title = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__title").text.strip()
+                vehicle_subtitle = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__subtitle").text.strip()
+                vehicle_price = vehicle.find_element(by=By.CLASS_NAME, value="product-card-pricing__price").text.strip()
+                vehicle_location = vehicle.find_elements(by=By.XPATH, value=".//span[@class='product-card-seller-info__spec-item-copy']")[-1].text
+                
+                vehicle_list.append({
+                    "id"   : vehicle_id,
+                    "uuid" : uuid4(),
+                    "data" : {
+                        "href"     : vehicle_href,
+                        "title"    : vehicle_title, 
+                        "subtitle" : vehicle_subtitle,
+                        "price"    : vehicle_price,
+                        "location" : vehicle_location
+                        }
+                    })
+        return vehicle_list
+    def __parse_vehicle_page(self, vehicle_url):
+        self.driver.get(vehicle_url)
+        self.__sleep(1)
+
+        img_track = self.driver.find_element(by=By.XPATH, value="//div[@class='slick-track']")
+        vehicle_img_list = []
+        vehicle_img_list.append(img_track.find_element(by=By.XPATH, value=".//img").get_attribute("src"))
+
+        vehicle_mileage = self.driver.find_element(by=By.XPATH, value="//span[@data-gui='mileage']").text.strip()
+
+        desc_button = self.driver.find_element(by=By.XPATH, value="//button[@class='sc-hQYpqk sc-feWZte sc-iyHPJt cDuRCe eFGeSa gexRyG atc-type-picanto atc-type-picanto--medium']")
+        desc_button.click()
+        vehicle_desc = self.driver.find_element(by=By.XPATH, value="//p[@class='sc-ffgBur byDqpY atc-type-picanto']").text.strip()
+        self.__sleep(0.2, 0)
+        desc_exit_button = self.driver.find_element(by=By.XPATH, value="//button[@aria-label='Close']")
+        desc_exit_button.click()
+
+        vehicle_data = {
+            "mileage" : vehicle_mileage,
+            "description" : vehicle_desc,
+            "img" : vehicle_img_list
+        }
+
+        return vehicle_data
+    
     def search_vehicle_type(self, make_type="Lotus", model_type="Elise", postcode="BA229SZ"):
 
         postcode_input = self.driver.find_element(by=By.XPATH, value="//input[@id='postcode']")
@@ -60,8 +113,8 @@ class Autotrader_scraper:
         self.__sleep(1)
 
         print(f"Searching for {make_type} {model_type}")
-    def get_url_list(self, max_pages=0):
-        vehicle_list = []
+    def get_vehicle_list(self, max_pages=0):
+
         search_url = self.driver.current_url[:-1]
 
         if max_pages == 0:
@@ -76,52 +129,15 @@ class Autotrader_scraper:
                 self.driver.get(f"{search_url}{page}")
             self.__sleep(1)
 
-            vehicles = self.driver.find_elements(by=By.XPATH, value="//li[@class='search-page__result']")
-
-            for vehicle in vehicles:
-                vehicle_standout = vehicle.find_elements(by=By.XPATH, value="span")
-                if vehicle_standout and vehicle_standout[0].text == 'Ad':
-                    continue
-                else:
-                    vehicle_href = vehicle.find_element(by=By.XPATH, value="article/a").get_attribute('href')
-                    vehicle_id = re.findall('[0-9]{15}', vehicle_href)[0]
-                    vehicle_title = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__title").text.strip()
-                    vehicle_subtitle = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__subtitle").text.strip()
-                    vehicle_price = vehicle.find_element(by=By.CLASS_NAME, value="product-card-pricing__price").text.strip()
-                    vehicle_location = vehicle.find_elements(by=By.XPATH, value=".//span[@class='product-card-seller-info__spec-item-copy']")[-1].text
-                    
-                    vehicle_list.append([vehicle_id, uuid4(), vehicle_href, vehicle_title, vehicle_subtitle, vehicle_price, vehicle_location])
-
-            print(f"{len(vehicles)} vehicles found on page {page}.")
+        vehicle_list = self.__parse_vehicle_list()
 
         return vehicle_list
-
-    def parse_vehicle_page(self, vehicle_url):
-        self.driver.get(vehicle_url)
-        self.__sleep(1)
-
-        img_track = self.driver.find_element(by=By.XPATH, value="//div[@class='slick-track']")
-        vehicle_img = img_track.find_element(by=By.XPATH, value=".//img").get_attribute("src")
-
-        vehicle_mileage = self.driver.find_element(by=By.XPATH, value="//span[@data-gui='mileage']").text.strip()
-
-        desc_button = self.driver.find_element(by=By.XPATH, value="//button[@class='sc-hQYpqk sc-feWZte sc-iyHPJt cDuRCe eFGeSa gexRyG atc-type-picanto atc-type-picanto--medium']")
-        desc_button.click()
-        vehicle_desc = self.driver.find_element(by=By.XPATH, value="//p[@class='sc-ffgBur byDqpY atc-type-picanto']").text.strip()
-        self.__sleep(0.2, 0)
-        desc_exit_button = self.driver.find_element(by=By.XPATH, value="//button[@aria-label='Close']")
-        desc_exit_button.click()
-
-        vehicle_data=[vehicle_mileage, vehicle_desc, vehicle_img]
-
-        return vehicle_data
     def add_vehicle_page_data(self, vehicle_list):
-        new_vehicle_list = []
-        for vehicle in vehicle_list:
-            data = self.parse_vehicle_page(vehicle[2])
-            new_vehicle_list.append(vehicle + data)
-        return new_vehicle_list
-
+        for i in range(len(vehicle_list)):
+            new_vehicle_data = self.__parse_vehicle_page(vehicle_list[i]['data']['href'])
+            vehicle_list[i]['data'].update(new_vehicle_data)
+            self.__sleep(0.5)
+        return vehicle_list
 
     def close(self):
         self.driver.close()
@@ -130,7 +146,9 @@ class Autotrader_scraper:
 
 if __name__ == "__main__":
     test = Autotrader_scraper()
-    test.accept_cookies()
     test.search_vehicle_type("Lotus", "Exige")
-    results = test.get_url_list()
+    results = test.get_vehicle_list(1)
+    print(results[0])
+    results = test.add_vehicle_page_data(results[:1])
+    print(results[0])
     test.close()
