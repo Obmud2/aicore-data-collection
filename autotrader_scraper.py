a7c8ae87c049a1cdd6498790927f8284ab40bb25
Jon Pring <obmud3@gmail.com>
2022-09-10 00:02:50 +0100

    Added parse method for individual vehicle pages

--- autotrader_scraper.py
+++ autotrader_scraper.py
@@ -6,7 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 import time
+import re
 from random import uniform
+from uuid import uuid4
 
 class Autotrader_scraper:
     def __init__(self):
@@ -21,7 +23,6 @@ class Autotrader_scraper:
 
     def __sleep(self, duration = 1, distribution=0.2):
         time.sleep(duration + uniform(-distribution, distribution))
-
     def accept_cookies(self):
         """
         Accept cookies on Autotrader homepage
@@ -35,10 +36,9 @@ class Autotrader_scraper:
             print("Cookies accepted")
             self.__sleep(1)
         except:
-            print(f"Error accepting cookies")
-    
+            print(f"Error accepting cookies")  
     def search_vehicle_type(self, make_type="Lotus", model_type="Elise", postcode="BA229SZ"):
-        
+
         postcode_input = self.driver.find_element(by=By.XPATH, value="//input[@id='postcode']")
         postcode_input.click()
         postcode_input.send_keys(postcode)
@@ -60,9 +60,8 @@ class Autotrader_scraper:
         self.__sleep(1)
 
         print(f"Searching for {make_type} {model_type}")
-
     def get_url_list(self, max_pages=0):
-        results_list = []
+        vehicle_list = []
         search_url = self.driver.current_url[:-1]
 
         if max_pages == 0:
@@ -85,19 +84,44 @@ class Autotrader_scraper:
                     continue
                 else:
                     vehicle_href = vehicle.find_element(by=By.XPATH, value="article/a").get_attribute('href')
+                    vehicle_id = re.findall('[0-9]{15}', vehicle_href)[0]
                     vehicle_title = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__title").text.strip()
                     vehicle_subtitle = vehicle.find_element(by=By.CLASS_NAME, value="product-card-details__subtitle").text.strip()
                     vehicle_price = vehicle.find_element(by=By.CLASS_NAME, value="product-card-pricing__price").text.strip()
                     vehicle_location = vehicle.find_elements(by=By.XPATH, value=".//span[@class='product-card-seller-info__spec-item-copy']")[-1].text
-                    results_list.append([vehicle_href, vehicle_title, vehicle_subtitle, vehicle_price, vehicle_location])
+                    
+                    vehicle_list.append([vehicle_id, uuid4(), vehicle_href, vehicle_title, vehicle_subtitle, vehicle_price, vehicle_location])
 
             print(f"{len(vehicles)} vehicles found on page {page}.")
 
-        return results_list
+        return vehicle_list
 
+    def parse_vehicle_page(self, vehicle_url):
+        self.driver.get(vehicle_url)
+        self.__sleep(1)
 
-    def get_vehicle_data(self, vehicle_url):
-        pass
+        img_track = self.driver.find_element(by=By.XPATH, value="//div[@class='slick-track']")
+        vehicle_img = img_track.find_element(by=By.XPATH, value=".//img").get_attribute("src")
+
+        vehicle_mileage = self.driver.find_element(by=By.XPATH, value="//span[@data-gui='mileage']").text.strip()
+
+        desc_button = self.driver.find_element(by=By.XPATH, value="//button[@class='sc-hQYpqk sc-feWZte sc-iyHPJt cDuRCe eFGeSa gexRyG atc-type-picanto atc-type-picanto--medium']")
+        desc_button.click()
+        vehicle_desc = self.driver.find_element(by=By.XPATH, value="//p[@class='sc-ffgBur byDqpY atc-type-picanto']").text.strip()
+        self.__sleep(0.2, 0)
+        desc_exit_button = self.driver.find_element(by=By.XPATH, value="//button[@aria-label='Close']")
+        desc_exit_button.click()
+
+        vehicle_data=[vehicle_mileage, vehicle_desc, vehicle_img]
+
+        return vehicle_data
+    def add_vehicle_page_data(self, vehicle_list):
+        new_vehicle_list = []
+        for vehicle in vehicle_list:
+            data = self.parse_vehicle_page(vehicle[2])
+            new_vehicle_list.append(vehicle + data)
+        return new_vehicle_list
+
 
     def close(self):
         self.driver.close()
