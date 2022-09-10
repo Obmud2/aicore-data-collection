caa41b23f826f5d3b6a75c75ee39c4582e74e458
Jon Pring <obmud3@gmail.com>
2022-09-10 21:48:44 +0100

    Docstrings added. test_search_vehicle_type() URL check improved to check search terms.

--- test/test_autotrader_scraper.py
+++ test/test_autotrader_scraper.py
@@ -8,16 +8,24 @@ def print_test_result(is_pass, test_case_name):
     print(f"Test case: {test_case_name} --> {'PASS' if is_pass else 'FAIL'}")
 
 def test_search_vehicle_type():
+    """
+    Check that search is completed and new url is reached including search terms
+    """
     with Timer():
+        test_make = "Lotus"
+        test_model = "Exige"
         test_scraper = Autotrader_scraper()
         init_url = test_scraper.driver.current_url[:-1]
-        test_scraper.search_vehicle_type("Lotus", "Elise")
+        test_scraper.search_vehicle_type(test_make, test_model)
         new_url = test_scraper.driver.current_url[:-1]
         test_scraper.close_session()
-        is_pass = init_url != new_url
+        is_pass = init_url != new_url and test_make in new_url and test_model in new_url
         print_test_result(is_pass, "test_search_vehicle_type()")
 
 def test_get_vehicle_list():
+    """
+    Check that number of listed vehicles matches number of results scraped.
+    """
     def scrape_num_results(test_search_results_url):
         page = urllib.request.urlopen(test_search_results_url)
         soup = BeautifulSoup(page, 'html.parser')
@@ -32,5 +40,5 @@ def test_get_vehicle_list():
         print_test_result(is_pass, "test_get_vehicle_list()")
 
 if __name__ == "__main__":
-    #test_search_vehicle_type()
+    test_search_vehicle_type()
     test_get_vehicle_list()
