888f27f4eaa25928164d19be56f493bb0e772d0c
Jon Pring <obmud3@gmail.com>
2022-09-10 19:58:35 +0100

    Unused dependency removed

--- scraper/autotrader_scraper.py
+++ scraper/autotrader_scraper.py
@@ -1,9 +1,8 @@
 from selenium import webdriver
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support.ui import Select
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 from random import uniform
 from timer import Timer
