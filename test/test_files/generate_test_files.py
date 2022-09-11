224effd0f011ad8606e6f92f24d2706401ad2681
Jon Pring <obmud3@gmail.com>
2022-09-11 18:42:18 +0100

    Added JSON test files to remove unit test dependencies

---
+++ test/test_files/generate_test_files.py
@@ -0,0 +1,27 @@
+
+import json
+from scraper.autotrader_scraper import Autotrader_scraper
+from scraper.vehicle_data import Vehicle_data
+
+if __name__ == "__main__":
+    scraper = Autotrader_scraper()
+    scraper.search_vehicle_type("Lotus", "Elise")
+    vehicle_data_list = scraper.get_vehicle_list(max_pages = 1)
+    vehicle_data_list_json = []
+    for vehicle in vehicle_data_list:
+        vehicle_data_list_json.append(vehicle.get_data())
+
+    json_object = json.dumps(vehicle_data_list_json, indent=4)
+    with open(f"test/test_files/initial_vehicle_data_list.json", 'w') as of:
+        of.write(json_object)
+
+    vehicle_data_list = scraper.add_vehicle_page_data(vehicle_data_list[:3])
+    vehicle_data_list_json = []
+    for vehicle in vehicle_data_list:
+        vehicle_data_list_json.append(vehicle.get_data())
+
+    json_object = json.dumps(vehicle_data_list_json, indent=4)
+    with open(f"test/test_files/full_vehicle_data_list.json", 'w') as of:
+        of.write(json_object)
+
+    scraper.close_session()
